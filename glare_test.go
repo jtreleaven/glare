a9803b16a0554d029c43ee936bfc975321154ef3
Jeff Treleaven <jtreleaven@redventures.com>
2016-07-11 14:06:23 -0400

    initial commit for unit testing.

---
+++ glare_test.go
@@ -0,0 +1,45 @@
+package glare
+
+import (
+    "net/http"
+    "testing"
+    "reflect"
+    "github.com/jarcoal/httpmock"
+)
+
+// TestGetConversationsByUserSuccess should give a user id to the method
+// and successfully receive a list of conversations
+func TestGetConversationsByUserSuccess(t testing.T) error {
+    var mockResult []Conversation
+    mockResult = append(mockResult, Conversation{ID: "1", URL: "www.weeee.com", MessagesURL: "layer:///messages/sdfkjasdlfkj", Participants: []string{"A", "B"}})
+    mockResult = append(mockResult, Conversation{ID: "2", URL: "localhost", MessagesURL: "layer:///messages/sdkfjlskdjflfkj", Participants: []string{"B", "C"}})
+
+    httpmock.Activate()
+    defer httpmock.DeactivateAndReset()
+
+    // mock to list out the articles
+	httpmock.RegisterResponder("GET", "https://api.layer.com/apps/123/users/B/conversations",
+		func(req *http.Request) (*http.Response, error) {
+			resp, err := httpmock.NewJsonResponse(200, mockResult)
+			if err != nil {
+				return httpmock.NewStringResponse(500, ""), nil
+			}
+			return resp, nil
+		},
+	)
+
+    l := New("123", "fjghfjshryfbus", "1.0")
+    convos, err := l.GetConversationsByUser("B")
+    if err != nil {
+        t.Log(err)
+        t.Fail()
+    }
+
+    if !reflect.DeepEqual(convos, mockResult) {
+        t.Log("Handled response is different that response given...")
+        t.Fail()
+    }
+
+    t.Log("Success!")
+    return nil
+}
