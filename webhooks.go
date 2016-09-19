8f881f0107ad502d453b2904a56155d1459d787c
Jeff Treleaven <jtreleaven@redventures.com>
2016-09-19 15:35:17 -0400

    updating gitignore and fixing old struct values in webhooks to more closely match the api.

--- webhooks.go
+++ webhooks.go
@@ -6,16 +6,15 @@ import (
 
 // WebHook defines the webhook resource available in the Layer API.
 type WebHook struct {
-	ID           string                 `json:"id"`
-	URL          string                 `json:"url"`
-	Status       string                 `json:"status"`
-	StatusReason string                 `json:"status_reason"`
-	CreatedAt    time.Time              `json:"created_at"`
-	Version      string                 `json:"version"`
-	TargetURL    string                 `json:"target_url"`
-	Events       []string               `json:"events"`
-	Secret       string                 `json:"secret"`
-	Config       map[string]interface{} `json:"config"`
+	ID        string                 `json:"id"`
+	URL       string                 `json:"url"`
+	Status    string                 `json:"status"`
+	CreatedAt time.Time              `json:"created_at"`
+	Version   string                 `json:"version"`
+	TargetURL string                 `json:"target_url"`
+	Events    []string               `json:"events"`
+	Secret    string                 `json:"secret"`
+	Config    map[string]interface{} `json:"config,omitempty"`
 }
 
 // A WebHookMessagePayload represents the request body sent from a Layer webhook.
