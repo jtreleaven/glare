291a01c49d6cc9ff2e0274096d1a19f0499e10a9
Ted Piecora <tpiecora@gmail.com>
2017-01-05 16:21:52 -0500

    Fix a couple places where the wrong parameters were getting passed in to the GET function.  Change time.Time to a pointer in the structs so that if it happens to come back as null from Layer it doesnt blow up when it gets unmarshaled. Applied gofmt to a couple files that didnt have it.

--- identity.go
+++ identity.go
@@ -2,11 +2,11 @@ package glare
 
 // Identity represents a single user resource from the Layer API
 type Identity struct {
-    DisplayName     string                  `json:"display_name"`
-    AvatarURL       string                  `json:"avatar_url"`
-    FirstName       string                  `json:"first_name"`
-    LastName        string                  `json:"last_name"`
-    Phone           string                  `json:"phone_number"`
-    Email           string                  `json:"email_address"`
-    MetaData        map[string]interface{}  `json:"metadata"`
+	DisplayName string                 `json:"display_name"`
+	AvatarURL   string                 `json:"avatar_url"`
+	FirstName   string                 `json:"first_name"`
+	LastName    string                 `json:"last_name"`
+	Phone       string                 `json:"phone_number"`
+	Email       string                 `json:"email_address"`
+	MetaData    map[string]interface{} `json:"metadata"`
 }
